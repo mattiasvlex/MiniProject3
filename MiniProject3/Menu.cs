a8fdd4b5432278ae833a5d862c1b4f1e2897fda5
mattiasvlex <mattias.vinje@gmail.com>
2024-04-05 12:09:36 +0200

    Added printing to program and menu

--- MiniProject3/Menu.cs
+++ MiniProject3/Menu.cs
@@ -8,5 +8,40 @@ namespace MiniProject3
 {
     internal class Menu
     {
+        public static string GetCommand()
+        {
+            Console.Write("(1): Print assets\n(2): Add asset\n(3): Quit\n\nChoose command: ");
+            string? command = Console.ReadLine();
+            return command ?? "";
+        }
+
+        public static string GetName()
+        {
+            Console.Write("");
+            string? name = Console.ReadLine();
+            return name ?? "";
+        }
+
+        public static void PrintError(string error)
+        {
+            Console.WriteLine("Invalid input: " + error);
+        }
+        public static void PrintAll(List<Asset> list, List<Asset> old)
+        {
+            
+            foreach (var o in old)
+            {
+                Console.WriteLine(o.GetType().Name + "   " + o.Office.Country + "   " + o.Date);
+            }
+        }
+        public static void PrintAdded()
+        {
+
+        }
+        public static void Quit()
+        {
+
+        }
+
     }
 }
--- MiniProject3/Program.cs
+++ MiniProject3/Program.cs
@@ -1,6 +1,8 @@
 ﻿using MiniProject3;
 using System.Diagnostics;
 using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
 using System.Runtime.Intrinsics.X86;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
@@ -12,11 +14,39 @@ List<Office> offices = new List<Office> {
 
 List<Asset> assets = new List<Asset> {
     new Computer("ASUS ROG", "B550-F", new DateOnly(2020, 11, 24), 9500, offices[0]),
-    new Computer("HP", "14S-FQ1010NO", new DateOnly(2022, 01, 30), 6790, offices[1]),
-    new Phone("Samsung", "S20 Plus", new DateOnly(2020, 09, 12), 700, offices[2]),
-    new Phone("Sony Xperia", "10 III", new DateOnly(2020, 03, 06), 8000, offices[0]),
+    new Computer("HP", "14S-FQ1010NO", new DateOnly(2023, 01, 30), 6790, offices[1]),
+    new Phone("Samsung", "S20 Plus", new DateOnly(2022, 09, 12), 700, offices[2]),
+    new Phone("Sony Xperia", "10 III", new DateOnly(2021, 03, 06), 8000, offices[0]),
     new Phone("Iphone", "10", new DateOnly(2018, 11, 25), 7500, offices[1]),
-    new Computer("HP", "Elitebook", new DateOnly(2021, 08, 30), 1530, offices[2]),
-    new Computer("HP", "Elitebook", new DateOnly(2020, 07, 30), 16450, offices[0])
+    new Computer("HP", "Elitebook", new DateOnly(2023, 08, 30), 1530, offices[2]),
+    new Computer("HP", "Elitebook", new DateOnly(2021, 07, 30), 16450, offices[0])
 };
 
+
+DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+
+string? input = Menu.GetCommand();
+
+while (EvaluateInput(input))
+{
+    input = Menu.GetCommand();
+}
+
+bool EvaluateInput(string input)
+{
+    switch (input)
+    {
+        case "1":  //Print
+            Menu.PrintAll(assets.OrderBy(a => a.GetType().Name).ThenBy(d => d.Office.Country).ToList(), 
+                assets.Where(d => (d.Date.Year <= today.Year - 3) || (d.Date.Month >= today.Month - 3 && d.Date.Year == today.Year - 3)).ToList());
+            return true;
+        case "2":  //Add
+
+            return true;
+        case "3":  //Quit
+            return false;
+        default:   //Try again
+            Menu.PrintError(input);
+            return true;
+    }
+}
