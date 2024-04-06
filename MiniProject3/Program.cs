05e024b712f312e113b84f457d87cd027cdec345
mattiasvlex <mattias.vinje@gmail.com>
2024-04-06 11:20:20 +0200

    Modified calculation of which assets to print

--- MiniProject3/Program.cs
+++ MiniProject3/Program.cs
@@ -19,11 +19,12 @@ List<Asset> assets = new List<Asset> {
     new Phone("Sony Xperia", "10 III", new DateOnly(2021, 03, 06), 8000, offices[0]),
     new Phone("Iphone", "10", new DateOnly(2018, 11, 25), 7500, offices[1]),
     new Computer("HP", "Elitebook", new DateOnly(2023, 08, 30), 1530, offices[2]),
-    new Computer("HP", "Elitebook", new DateOnly(2021, 07, 30), 16450, offices[0])
+    new Computer("HP", "Elitebook", new DateOnly(2021, 07, 01), 16450, offices[0])
 };
 
 
 DateOnly today = DateOnly.FromDateTime(DateTime.Now);
+int todayInMonths = today.Year * 12 + today.Month;  //Easier to calculate difference in months instead of years and months
 
 string? input = Menu.GetCommand();
 
@@ -38,12 +39,19 @@ bool EvaluateInput(string input)
     {
         case "1":  //Print
             Menu.PrintAll(assets.OrderBy(a => a.GetType().Name).ThenBy(d => d.Office.Country).ToList(), 
-                assets.Where(d => (d.Date.Year <= today.Year - 3) || (d.Date.Month >= today.Month - 3 && d.Date.Year == today.Year - 3)).ToList());
+                assets.Where(d => (todayInMonths - (d.Date.Year * 12 + d.Date.Month) == 33 && today.Day > d.Date.Day)
+				|| todayInMonths - (d.Date.Year * 12 + d.Date.Month) >= 34).ToList());  //3*12 + 9 == 33 months
             return true;
         case "2":  //Add
-
+            //Ask for type --> ask for brand, model.
+			
+            //Ask for price.
+            //Ask for office --> check if exists, otherwise ask for country, currency, toUSD.
+            // --> create new asset
+            //if (assets.Add()) {Menu.PrintAdded(); return true; else {return false}}
             return true;
         case "3":  //Quit
+            Menu.Quit();
             return false;
         default:   //Try again
             Menu.PrintError(input);
