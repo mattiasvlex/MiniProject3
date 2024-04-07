fb8c4b253145d4c0919b34dc5cbc31867f33f342
mattiasvlex <mattias.vinje@gmail.com>
2024-04-07 12:35:25 +0200

    Added check on price field, and added method to calculate local price

--- MiniProject3/Asset.cs
+++ MiniProject3/Asset.cs
@@ -8,8 +8,26 @@ namespace MiniProject3
 {
     internal class Asset
     {
+        private int _price;
+
+        public int Price {
+
+            get => _price;
+
+            set
+            {
+                if (value < 0)
+                {
+                    _price = 0;
+                }
+                else
+                {
+                    _price = value;
+                }
+            }
+         
+        }
         public DateOnly Date { get; set; }
-        public int Price { get; set; }
         public Office Office { get; set; }
 
         public Asset(DateOnly date, int price, Office office)
@@ -18,6 +36,9 @@ namespace MiniProject3
             Price = price;
             Office = office;
         }
-
+        protected decimal ToLocal()
+        {
+            return this.Price * this.Office.ToLocal;
+        }
     }
 }
